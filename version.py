e8b5ade026f9d3d811ea5461e25718186d990b9f
marvin1309 <marvin@fam-feser.de>
2026-05-23 04:07:17 +0200

    added ci file

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Repository version metadata for container publishing."""
 
-__version__ = "0.0.1"
-__version_info__ = (0, 0, 1)
+__version__ = "0.0.2"
+__version_info__ = (0, 0, 2)
 
 
 def get_version() -> str:
